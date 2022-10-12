51c3c4e6b8dd7c547a57b8b0f1fe21d91d26cbd8
Avatar Ai <113794392+AvatarsAi@users.noreply.github.com>
2022-10-12 12:17:15 +0100

    Add files via upload

---
+++ number_line.cpp
@@ -0,0 +1,57 @@
+namespace pulgaCpp{ //Generate namespace library
+    inline namespace version_1{ //"local" namespace library
+        class number_line{
+            signed long long int pointer{}; // "pointer" invariant for pulgacpp starts at zero
+
+
+        public:
+            constexpr number_line()=default;
+            constexpr explicit number_line(long long arg):pointer{arg}{}
+
+
+            template<class X> operator X()=delete;
+            operator <<(long long arg){return pointer -= arg;}
+            operator >>(long long arg){ return pointer += arg;}
+            operator()(long long arg){return pointer += arg;}
+            operator *(){return pointer;}
+            operator==(long long arg){return (pointer == arg)?true:false;}
+            operator==(number_line&arg){return (pointer == *arg)?true:false;}
+            operator==(number_line&&arg){return (pointer == *arg)?true:false;}
+
+            operator!=(long long arg){return (pointer != arg)?true:false;}
+            operator!=(number_line&arg){return (pointer != *arg)?true:false;}
+            operator!=(number_line&&arg){return (pointer != *arg)?true:false;}
+
+
+            operator<(long long arg){return (pointer < arg)?true:false;}
+            operator<(number_line&arg){return (pointer < *arg)?true:false;}
+            operator<(number_line&&arg){return (pointer < *arg)?true:false;}
+
+            operator>(long long arg){return (pointer > arg)?true:false;}
+            operator>(number_line&arg){return (pointer > *arg)?true:false;}
+            operator>(number_line&&arg){return (pointer > *arg)?true:false;}
+
+            operator>=(long long arg){return (pointer >= arg)?true:false;}
+            operator>=(number_line&arg){return (pointer >= *arg)?true:false;}
+            operator>=(number_line&&arg){return (pointer >= *arg)?true:false;}
+
+
+            operator<=(long long arg){return (pointer <= arg)?true:false;}
+            operator<=(number_line&arg){return (pointer <= *arg)?true:false;}
+            operator<=(number_line&&arg){return (pointer <= *arg)?true:false;}
+
+
+
+            auto get(){ return this->pointer; }
+            auto max(){ return std::numeric_limits<long long>::max(); }
+            auto min(){ return std::numeric_limits<long long>::min();}
+            auto clear(){return pointer = 0;}
+            auto point(){return pointer;}
+            auto prev(int prev = 1){return pointer - prev;}
+            auto next(int next = 1 ){ return pointer + next;}
+
+        };
+
+    }
+
+}
